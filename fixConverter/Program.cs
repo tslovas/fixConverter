9ecd5ecce757212f7ffc5494ec484f7ba58b5cd1
Todd Lovas <toddlovas@quickenloans.com>
2019-09-12 15:53:08 -0400

    adding solution shell

---
+++ fixConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fixConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
